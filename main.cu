55739e5b066a1a7436c02d8112a736b713569b88
Gustavo Gutierrez <gusguma@gmail.com>
2022-11-02 20:26:50 +0100

    [arco-entregable-1] Initial Commit

---
+++ main.cu
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////
+/// PROGRAMACIÓN EN CUDA C/C++
+/// Práctica:	ENTREGABLE 1 : Temporización GPU
+/// Autor:		Angel Sierra Gomez, Gustavo Gutierrez Martin
+/// Fecha:		Noviembre 2022
+///////////////////////////////////////////////////////////////////////////
+
+/// dependencias ///
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cuda_runtime.h>
+
+/// constantes ///
+#define MB (1<<20) /// MiB = 2^20
+#define ROWS 7
+#define COLUMNS 24
+
+
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
